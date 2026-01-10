b8e8c12cbc95c16e241209f95782e76c6f706a67
MiroslavskyCoder <programmer_js@vk.com>
2026-01-10 08:26:42 +0300

    feat(dance): Implement MouthAnimator and MouthListener for player chat animations

--- packages/dance/src/main/java/com/webx/dance/DancePlugin.java
+++ packages/dance/src/main/java/com/webx/dance/DancePlugin.java
@@ -4,12 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DancePlugin extends JavaPlugin {
     private DanceManager danceManager;
+    private MouthAnimator mouthAnimator;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         danceManager = new DanceManager(this);
         getCommand("dance").setExecutor(new DanceCommand(danceManager));
+        mouthAnimator = new MouthAnimator(this);
+        getServer().getPluginManager().registerEvents(new MouthListener(this, mouthAnimator), this);
         getLogger().info("Dance 0.1.0 enabled - 20+ unique dances loaded");
     }
 
@@ -20,5 +23,9 @@ public class DancePlugin extends JavaPlugin {
 
     public DanceManager getDanceManager() {
         return danceManager;
+    }
+
+    public MouthAnimator getMouthAnimator(){
+        return mouthAnimator;
     }
 }
