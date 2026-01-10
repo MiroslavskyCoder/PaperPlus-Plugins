baf4262b9ad3d949d4d9e4c5363f8ca4bfcd0244
MiroslavskyCoder <programmer_js@vk.com>
2026-01-10 16:27:21 +0300

    feat(achievements): Initialize AchievementManager in onEnable and add getter method

--- packages/achievements/src/main/java/com/webx/achievements/AchievementsPlugin.java
+++ packages/achievements/src/main/java/com/webx/achievements/AchievementsPlugin.java
@@ -4,13 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class AchievementsPlugin extends JavaPlugin {
 
+    private AchievementManager achievementManager;
+
     @Override
     public void onEnable() {
+        this.achievementManager = new AchievementManager(this);
         getLogger().info("Achievements plugin enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Achievements plugin disabled!");
+    }
+
+    public AchievementManager getAchievementManager() {
+        return achievementManager;
     }
 }
