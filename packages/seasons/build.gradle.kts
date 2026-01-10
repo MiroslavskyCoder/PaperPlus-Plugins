545aecd45eb98ce41850668ad0f8132b91170634
MiroslavskyCoder <programmer_js@vk.com>
2026-01-10 16:22:47 +0300

    feat: Add build configurations for multiple packages with PaperMC dependencies

---
+++ packages/seasons/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    archiveBaseName.set("seasons")
+    archiveVersion.set("1.0.0")
+}
