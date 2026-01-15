bac50a812916f134b13214b41fba35a84f0b49ba
MiroslavskyCoder <programmer_js@vk.com>
2026-01-15 08:14:52 +0300

    Add build tasks and SimpleHeal module configuration

---
+++ packages/simpleheal/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "com.webx"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("com.google.code.gson:gson:2.10.1")
+    
+    // Shared database library
+    implementation(project(":common"))
+    implementation(project(":redis-storage"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(17)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
