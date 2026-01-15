bfc3fda31a15d6e1b2e9614b2d764d3afb0c722e
MiroslavskyCoder <programmer_js@vk.com>
2026-01-15 08:02:36 +0300

    refactor: Remove Java toolchain configuration from build.gradle.kts

--- redis-storage/build.gradle.kts
+++ redis-storage/build.gradle.kts
@@ -15,12 +15,6 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.release.set(17)
